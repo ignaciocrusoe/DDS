1b6891c226fd6bc6139bd8afdc7a849dc3fe11a3
Martin-Fedorenko <MartinFedorenkoLK1288@gmail.com>
2023-04-18 18:44:05 -0300

    prestamo

---
+++ 2023_04_12_Ej_31_Biblioteca/Prestamo.java
@@ -0,0 +1,41 @@
+import java.time.temporal.ChronoUnit;
+import java.time.LocalDateTime;
+
+public class Prestamo{
+    private Boolean devuelto = false;
+    private Boolean vencido = false;
+    //private Copia copiaPrestada;
+    //private Persona persona;
+    private LocalDateTime  fechaPrestamo;
+    private LocalDateTime  fechaDeDevolucion;
+    private LocalDateTime fechaVencimiento;
+
+    public Prestamo(/*Copia copiaPrestada*/){
+        //this.copiaPrestada = copiaPrestada;
+        this.fechaPrestamo = LocalDateTime.now();
+    }
+
+    public void recibirDevolucion(){
+        this.fechaDeDevolucion = LocalDateTime.now();
+        this.fechaVencimiento = this.fechaPrestamo.plusDays(30);
+        this.devuelto = true;
+    }
+
+    public void verificarPrestamo(){
+        if(this.fechaDeDevolucion.isAfter(this.fechaVencimiento)){
+            this.vencido = true;
+        }
+    }
+
+    public Integer calcularDiasDeMulta(){
+        Long diasAtrasados = this.fechaVencimiento.until(this.fechaDeDevolucion, ChronoUnit.DAYS);
+
+        Integer diasMulta = (int) (long) Math.max(0, diasAtrasados)*2;
+
+        return diasMulta;
+    }
+
+    public static void main(String[] args){
+        System.out.println(LocalDateTime.now());
+    }
+}
